a20b88bd4a0c37c49dfbbd789347370c964dd7a6
PantherDave <dacos062@fiu.edu>
2021-11-14 22:46:49 -0500

    Display list of profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -31,7 +31,7 @@ export const getCurrentProfile = () => async (dispatch) => {
 
 // Get all profiles
 export const getProfiles = () => async (dispatch) => {
-  dispatch({ CLEAR_PROFILE });
+  dispatch({ type: CLEAR_PROFILE });
 
   try {
     const res = await axios.get("/api/profile");
@@ -213,7 +213,7 @@ export const deleteEducation = (id) => async (dispatch) => {
 export const deleteAccount = () => async (dispatch) => {
   if (window.confirm("Are you sure? This CANNOT be undone!")) {
     try {
-      const res = await axios.delete(`api/profile/`);
+      await axios.delete(`api/profile/`);
 
       dispatch({ type: CLEAR_PROFILE });
 
